fa584448ad35af42c4464f89601d795084c23db9
ReemALHamed <reemsaadhamed@gmail.com>
2023-04-06 01:20:49 +0300

    fix

--- src/screens/FetchData.js
+++ src/screens/FetchData.js
@@ -8,7 +8,6 @@ export default function FetchData() {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(false);
   const [eventPerPage, setEventPerPage] = useState(12);
-  const [total, setTotal] = useState(0);
   const [pageCount, setPageCount] = useState(0);
 
   useEffect(() => {
@@ -17,14 +16,11 @@ export default function FetchData() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  var items: Item[] = null;
   const fetchEvents = async (index) => {
     setLoading(true);
     const { data } = await Axios.get(
       `https://api.riyadh.sa/api/CountedEvents?_format=json&page=${index}`
     );
-    items = data.result.items;
-    setTotal(data.result.counters.total);
     eventPerPage === 0 && setEventPerPage(data.result.items.length);
     setPageCount(Math.ceil(data.result.counters.total / eventPerPage));
     setEvents(data);
